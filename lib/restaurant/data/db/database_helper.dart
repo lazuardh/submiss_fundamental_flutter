3831ba92f43523e2863c620a19270457de7663a0
habiyanlazuard21 <habiyanlazuard21@gmail.com>
2024-04-12 15:01:50 +0700

    added database, provider favorite

--- lib/restaurant/data/db/database_helper.dart
+++ lib/restaurant/data/db/database_helper.dart
@@ -1,63 +1,80 @@
-// import 'package:sqflite/sqflite.dart';
-// import 'package:submiss1_fundamental/restaurant/data/model/restaurant_model.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:submiss1_fundamental/restaurant/data/model/restaurant_model.dart';
 
-// class DatabaseHelper {
-//   static DatabaseHelper? _instance;
-//   static Database? _database;
+class DatabaseHelper {
+  static DatabaseHelper? _instance;
+  static Database? _database;
 
-//   DatabaseHelper._internal() {
-//     _instance = this;
-//   }
+  DatabaseHelper._internal() {
+    _instance = this;
+  }
 
-//   factory DatabaseHelper() => _instance ?? DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance ?? DatabaseHelper._internal();
 
-//   static const String _tbFavorit = 'favorite';
+  static const String _tbFavorit = 'favorite';
 
-//   Future<Database> _initializeDb() async {
-//     var path = await getDatabasesPath();
-//     var db = openDatabase(
-//       '$path/restaurant.db',
-//       onCreate: (db, version) async {
-//         await db.execute('''CREATE TABLE $_tbFavorit (
-//               id TEXT PRIMARY KEY,
-//               name TEXT,
-//               description TEXT,
-//               picturedId TEXT,
-//               city TEXT,
-//               rating int,
-//             )
-//           ''');
-//       },
-//       version: 1,
-//     );
-//     return db;
-//   }
+  Future<Database> _initializeDb() async {
+    var path = await getDatabasesPath();
+    var db = openDatabase(
+      '$path/restaurant.db',
+      onCreate: (db, version) async {
+        await db.execute('''CREATE TABLE $_tbFavorit (
+              id TEXT PRIMARY KEY,
+              name TEXT,
+              description TEXT,
+              picturedId TEXT,
+              city TEXT,
+              rating int,
+            )
+          ''');
+      },
+      version: 1,
+    );
+    return db;
+  }
 
-//   Future<Database?> get database async {
-//     if (_database == null) {
-//       _database = await _initializeDb();
-//     }
+  Future<Database?> get database async {
+    if (_database == null) {
+      _database = await _initializeDb();
+    }
 
-//     return _database;
-//   }
+    return _database;
+  }
 
-//   Future<void> insertFavorite(Restaurant restaurant) async {
-//     final db = await database;
-//     await db!.insert(_tbFavorit, restaurant.toJson());
-//   }
+  Future<void> insertFavorite(Restaurant restaurant) async {
+    final db = await database;
+    await db!.insert(_tbFavorit, restaurant.toJson());
+  }
 
-//   Future<List<Restaurant>> getFavoriteRestaurant() async {
-//     final db = await database;
-//     List<Map<String, dynamic>> results = await db!.query(
-//       _tbFavorit,
-//       where: 'id = ?',
-//       whereArgs: [id],
-//     );
+  Future<List<Restaurant>> getFavoriteRestaurant() async {
+    final db = await database;
+    List<Map<String, dynamic>> results = await db!.query(_tbFavorit);
 
-//     if (results.isNotEmpty) {
-//       return results.first;
-//     } else {
-//       return {};
-//     }
-//   }
-// }
+    return results.map((response) => Restaurant.fromJson(response)).toList();
+  }
+
+  Future<Map> getFavoriteRestaurantById(String id) async {
+    final db = await database;
+    List<Map<String, dynamic>> results = await db!.query(
+      _tbFavorit,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (results.isNotEmpty) {
+      return results.first;
+    } else {
+      return {};
+    }
+  }
+
+  Future<void> removeFavoriteRestaurant(String id) async {
+    final db = await database;
+
+    await db!.delete(
+      _tbFavorit,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
--- lib/restaurant/ui/provider/restaurant_favorite.dart
+++ lib/restaurant/ui/provider/restaurant_favorite.dart
@@ -1,23 +1,61 @@
 import 'package:flutter/foundation.dart';
-import 'package:submiss1_fundamental/restaurant/data/preferences/preference_helper.dart';
+import 'package:submiss1_fundamental/restaurant/data/db/database_helper.dart';
+import 'package:submiss1_fundamental/restaurant/data/model/restaurant_model.dart';
+
+enum ResultState { loading, noData, hasData, error }
 
 class FavoriteRestaurantProvider extends ChangeNotifier {
-  PreferencesHelper preferencesHelper;
+  final DatabaseHelper databaseHelper;
 
-  FavoriteRestaurantProvider({required this.preferencesHelper}) {
-    _getFavoriteItem();
+  FavoriteRestaurantProvider({required this.databaseHelper}) {
+    _getFavorite();
   }
 
-  bool _isFavorite = false;
-  bool get isFavorite => _isFavorite;
+  late ResultState _state;
+  ResultState get state => _state;
 
-  void _getFavoriteItem() async {
-    _isFavorite = await preferencesHelper.isFavorite;
+  String _message = '';
+  String get message => _message;
+
+  List<Restaurant> _favorite = [];
+  List<Restaurant> get favorite => _favorite;
+
+  void _getFavorite() async {
+    _favorite = await databaseHelper.getFavoriteRestaurant();
+    if (_favorite.length > 0) {
+      _state = ResultState.hasData;
+    } else {
+      _state = ResultState.noData;
+      _message = 'Empty Data favorite';
+    }
     notifyListeners();
   }
 
-  void enableFavoriteItem(bool value) {
-    preferencesHelper.setFavorite(value);
-    _getFavoriteItem();
+  void addFavorite(Restaurant restaurant) async {
+    try {
+      await databaseHelper.insertFavorite(restaurant);
+      _getFavorite();
+    } catch (e) {
+      _state = ResultState.error;
+      _message = 'Error menambah data : $e';
+      notifyListeners();
+    }
+  }
+
+  Future<bool> isFavorited(String id) async {
+    final favoriteRestaurant =
+        await databaseHelper.getFavoriteRestaurantById(id);
+    return favoriteRestaurant.isNotEmpty;
+  }
+
+  void removeBookmark(String id) async {
+    try {
+      await databaseHelper.removeFavoriteRestaurant(id);
+      _getFavorite();
+    } catch (e) {
+      _state = ResultState.error;
+      _message = 'Error: $e';
+      notifyListeners();
+    }
   }
 }
